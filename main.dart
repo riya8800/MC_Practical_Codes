25d1f9a699292adb6975196dce8c89b6ce5e6f68
Renuka_joshi <110252357+riya8800@users.noreply.github.com>
2024-11-30 01:31:25 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+// main.dart
+
+import 'package:flutter/material.dart';
+import 'browser_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Browser',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BrowserScreen(),
+    );
+  }
+}
+
+
